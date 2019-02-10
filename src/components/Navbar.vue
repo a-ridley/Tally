2e17f57e5e43b00bae00c017f0712787371b723f
pwoosam <pwoosam5@icloud.com>
2019-02-10 14:08:06 -0800

    Fixed router

--- src/components/Navbar.vue
+++ src/components/Navbar.vue
@@ -1,7 +1,7 @@
 <template>
 <nav class="navbar" role="navigation" aria-label="main navigation">
   <div class="navbar-brand">
-    <a class="navbar-item" href="/#">
+    <a class="navbar-item" v-on:click="goHome()">
       <img src="@/assets/TallyLogo.png"  width="145" height="28">
     </a>
 
@@ -16,8 +16,12 @@
 
 <script lang="ts">
 import { Component, Vue } from "vue-property-decorator";
+import { Route } from "vue-router";
 
 @Component
 export default class Navbar extends Vue {
+  private goHome() {
+    this.$router.push({ name: "home"});
+  }
 }
 </script>
